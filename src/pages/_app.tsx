33a55944125d106436abeefdfe46531e1461b440
lucaoskaique <lucas.kaique175@hotmail.com>
2022-03-12 17:42:15 -0300

    themeprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>React/Nextjs - Boilerplate</title>
         <link rel="short icon" href="/img/icon-512.png"></link>
@@ -18,7 +20,7 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
