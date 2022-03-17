d2229e38956732ee6db5fb929c4ccd15d4695b78
lucaoskaique <lucas.kaique175@hotmail.com>
2022-03-17 10:42:45 -0300

    Created menu component

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react'
+
+import Menu from '.'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Default: Story = () => <Menu />
