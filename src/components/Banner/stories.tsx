a7aa31fc6c65a69fe4b8015757fdf5dedfe60ea6
lucaoskaique <lucas.kaique175@hotmail.com>
2022-03-18 16:08:04 -0300

    Created Banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react'
+
+import Banner from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner
+} as Meta
+
+export const Default: Story = () => <Banner />
