35cc905aefcebbd533ad0f51bf54405b61992ae9
lucaoskaique <lucas.kaique175@hotmail.com>
2022-03-18 20:52:05 -0300

    Created TextField component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react'
+
+import TextField from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField
+} as Meta
+
+export const Default: Story = () => <TextField />
