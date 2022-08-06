2e1ba5630a72cbddeb8df0b7ab6077ad423c6b22
lucaoskaique <lucas.kaique175@hotmail.com>
2022-08-06 19:50:33 -0300

    Created folder to graphql queries

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,31 +1,6 @@
-import { gql } from '@apollo/client'
+import { QUERY_NEWS } from 'graphql/queries/news'
 import NewsPageTemplate, { NewsPageTemplateProps } from 'templates/NewsPage'
 import { initializeApollo } from 'utils/apollo'
-
-const QUERY_NEWS = gql`
-  query getNews {
-    posts {
-      data {
-        id
-        attributes {
-          title
-          slug
-          short_description
-          content
-          cover {
-            data {
-              attributes {
-                name
-                url
-              }
-            }
-          }
-          publishedAt
-        }
-      }
-    }
-  }
-`
 
 export default function NewsPage(props: NewsPageTemplateProps) {
   // if (props) return <p>{JSON.stringify(props, null, 2)}</p>
