ec9022e46de80109f5812944b7f13f25a9565b30
lucaoskaique <lucas.kaique175@hotmail.com>
2022-08-06 19:46:15 -0300

    appollo configs in news

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,15 +1,74 @@
+import { gql } from '@apollo/client'
 import NewsPageTemplate, { NewsPageTemplateProps } from 'templates/NewsPage'
+import { initializeApollo } from 'utils/apollo'
 
-import newsMock from '../templates/NewsPage/mock'
+const QUERY_NEWS = gql`
+  query getNews {
+    posts {
+      data {
+        id
+        attributes {
+          title
+          slug
+          short_description
+          content
+          cover {
+            data {
+              attributes {
+                name
+                url
+              }
+            }
+          }
+          publishedAt
+        }
+      }
+    }
+  }
+`
 
 export default function NewsPage(props: NewsPageTemplateProps) {
+  // if (props) return <p>{JSON.stringify(props, null, 2)}</p>
   return <NewsPageTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({ query: QUERY_NEWS })
+
+  if (data === null) {
+    return {
+      props: {}
+    }
+  }
+
+  const news = data.posts.data.map((post) => ({
+    title: post.attributes.title,
+    description: post.attributes.short_description,
+    slug: post.attributes.slug,
+    content: post.attributes.content,
+    img: `http://localhost:1337${post.attributes.cover.data.attributes.url}`,
+    date: new Intl.DateTimeFormat('pt-BR', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    }).format(new Date(post.attributes.publishedAt))
+  }))
+
   return {
     props: {
-      news: newsMock
+      revalidate: 60,
+      news: news
     }
   }
 }
+
+// {
+//   title: 'DIREITO Á MORADIA',
+//   slug: 'democracia-teste',
+//   description:
+//     'Moradores da Vila Nazaré pedem que CCDH acompanhe processo de realocação das famílias',
+//   img: 'https://source.unsplash.com/user/willianjusten/1300x640',
+//   date: '28 de novembro de 2021'
+// }
